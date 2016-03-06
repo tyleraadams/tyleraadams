2d22d1608cb2fe66c1aa4aebed87e30583b9f9a4
Tyler Adams <tyleraadams@gmail.com>
2016-03-05 22:27:17 -0500

    updated homeapge copy and made js oo

--- js/main.js
+++ js/main.js
@@ -1,26 +1,117 @@
+var objectSelector = function (selector) {
+    return {
+        selectDomFromSelector: function selectDomFromSelector() {
+            return document.querySelector(selector);
+        }
+    };
+};
+
+var Scroller = function (arrowQuery, toWhere) {
+  var arrow = objectSelector(arrowQuery).selectDomFromSelector();
+  var toWhereDom = objectSelector(toWhere).selectDomFromSelector()
+  function init () {
+    arrow.addEventListener('click', scroll.call(this, toWhereDom));
+
+  };
+  function scroll (toWhere) {
+    return function (e) {
+
+        scrollTo(portfolio.offsetTop, 500, easing.linear);
+    }
+  };
+
+  return  { init: init };
+
+};
+
+var Portfolio = function (portfolioSelector) {
+
+    var dom = objectSelector(portfolioSelector).selectDomFromSelector();
+    var myEntries = [];
+    var isOpen;
+
+    function init () {
+        buildPortfolioEntryList(dom);
+        dom.addEventListener('click', handleClick);
+    };
+    function buildPortfolioEntryList (portfolioDom) {
+        var portfolioItems = portfolioDom.querySelectorAll('li');
+        for (var i=0; i < portfolioItems.length; i++) {
+            var portfolioItemDom = portfolioItems[i];
+            var portfolioEntry = PortfolioEntry(portfolioItemDom, i);
+            myEntries.push(portfolioEntry)
+            portfolioEntry.init();
+        }
+    };
+    function handleClick (e) {
+        var index = 0;
+        clickEntryIndex = getClosest(e.target, 'li').dataset.index;
+        var clickedEntry = myEntries[clickEntryIndex];
+        if (clickedEntry.amIOpen()) {
+            return clickedEntry.close();
+        }
+        for (index; index < myEntries.length; index++) {
+            var portfolioEntry = myEntries[index];
+            if (portfolioEntry.amIOpen()) {
+                portfolioEntry.close();
+            }
+        }
+        if (!clickedEntry.amIOpen()) {
+            clickedEntry.open()
+        }
+    };
+    return {
+        init: init
+    };
+};
+
+var PortfolioEntry = function (portfolioItemDom, i) {
+    var myDom = portfolioItemDom;
+    var teaser = myDom.querySelector('.teaser');
+    var arrow = myDom.querySelector('.fa-angle-right');
+    var myIndex = i;
+    var isOpen = false;
+    function init () {
+        myDom.dataset.index = myIndex;
+    }
+    function amIOpen() {
+        return isOpen;
+    }
+    function close () {
+        myDom.classList.remove('open');
+        teaser.classList.add('hidden');
+        arrow.classList.remove('fa-angle-down');
+        isOpen = false;
+    }
+    function open () {
+        myDom.classList.add('open');
+        teaser.classList.remove('hidden');
+        arrow.classList.add('fa-angle-down');
+        isOpen = true;
+    }
+    function getDom (){
+        return myDom;
+    }
+    return {
+        init: init,
+        open: open,
+        close: close,
+        getDom: getDom,
+        amIOpen: amIOpen
+    }
+};
+
+
 
 document.addEventListener("DOMContentLoaded", function(event) {
-  var arrow = document.querySelector('.go-down');
-  var portfolio = document.querySelector('#portfolio');
-  function clickHandler (e) {
-    scrollTo(portfolio.offsetTop, 500, easing.linear);
-  }
-  arrow.addEventListener('click', clickHandler);
+  var arrowQuery = '.go-down';
+  var portfolioQuery = '#portfolio';
+  var portfolio = Portfolio(portfolioQuery);
+  var scroller = Scroller(arrowQuery, portfolioQuery);
 
-  var portfolioItems = document.querySelectorAll('#portfolio li');
-  for (var i=0; i < portfolioItems.length; i++){
-    var portfolioItem = portfolioItems[i];
-    portfolioItem.addEventListener('click', function(event){
-      this.querySelector('.teaser').classList.toggle('hidden');
-      this.querySelector('.fa-angle-right').classList.toggle('fa-angle-down');
-
-      if (!hasClass(this.querySelector('.teaser'), 'hidden')) {
-        scrollTo(portfolioItem.offsetTop + portfolioItem.offsetHeight, 500, easing.linear);
-      }
-    });
-  }
+  portfolio.init();
+  scroller.init();
 });
-
 // Helper functions
 
 function hasClass(el, cls) {
@@ -92,3 +183,43 @@ var easing = {
   // acceleration until halfway, then deceleration
   easeInOutQuint: function (t) { return t<.5 ? 16*t*t*t*t*t : 1+16*(--t)*t*t*t*t }
 }
+
+
+var getClosest = function (elem, selector) {
+
+    var firstChar = selector.charAt(0);
+
+    // Get closest match
+    for ( ; elem && elem !== document; elem = elem.parentNode ) {
+
+        // If selector is a class
+        if ( firstChar === '.' ) {
+            if ( elem.classList.contains( selector.substr(1) ) ) {
+                return elem;
+            }
+        }
+
+        // If selector is an ID
+        if ( firstChar === '#' ) {
+            if ( elem.id === selector.substr(1) ) {
+                return elem;
+            }
+        }
+
+        // If selector is a data attribute
+        if ( firstChar === '[' ) {
+            if ( elem.hasAttribute( selector.substr(1, selector.length - 2) ) ) {
+                return elem;
+            }
+        }
+
+        // If selector is a tag
+        if ( elem.tagName.toLowerCase() === selector ) {
+            return elem;
+        }
+
+    }
+
+    return false;
+
+};
